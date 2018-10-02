e7179128b7b6cf715932df60cb773e9cd43ea2ac
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-02 15:35:23 -0600

    Added Start and How to Play buttons

--- js/app.js
+++ js/app.js
@@ -7,4 +7,8 @@ $('#score').text("Score: " + score + "");
 $('.lifecount').append("<span id='lives'>Lives: (0)</span>");
 $('#lives').text("Lives: " + lives + "");
 
-
+// Start Game
+$("#start").on("click", (e) => {
+    $("img").attr("src", "Project-1-images/Show-Me-What-You-Got.gif")
+    draw();
+  });

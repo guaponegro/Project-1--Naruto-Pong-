b49d548c13b8976ab0bb8dcc0e7241eb5d123fc9
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-02 16:36:55 -0600

    Added a backgroundand centered the title

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 // Scoreboard
-$('.scoreboard').append("<span id='score'>Score: (0)</span>");
-$('#score').text("Score: " + score + "");
+$('.scoreboard').append("<span class='score'>Score: (0)</span>");
+$('.score').text("Score: " + score + "");
 
 
 // Lives Tracker
-$('.lifecount').append("<span id='lives'>Lives: (0)</span>");
-$('#lives').text("Lives: " + lives + "");
+$('.lifecount').append("<span class='lives'>Lives: (0)</span>");
+$('.lives').text("Lives: " + lives + "");
 
 // Start Game
 $("#start").on("click", (e) => {
-    $("img").attr("src", "Project-1-images/Show-Me-What-You-Got.gif")
+
     draw();
   });
+
+  
+
+$("#col-right").append("<span id='img'></span>");
+$("#img").attr("src", "../images/Rick-Dancing.gif")
